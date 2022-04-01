632d88dd14cd3d000d2d7c649fa7cec205e1a0c1
SAJITH P I <sajithpalamanna@gmail.com>
2022-04-01 21:33:27 +0530

    Procfile

--- til/settings.py
+++ til/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','socialbook79.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','socilbook7.herokuapp.com']
 
 
 
